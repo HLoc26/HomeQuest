4ac49f480a5b1572c26db52dedfb23be25bcf1bc
HLoc26 <dhl26052004@gmail.com>
2025-02-16 22:31:54 +0700

    update: set force and alter to false to avoid severals indexing

--- src/config/syncdb.js
+++ src/config/syncdb.js
@@ -2,7 +2,7 @@ import sequelize from "./db.js";
 
 const syncDatabase = async () => {
     try {
-        await sequelize.sync({ alter: true })
+        await sequelize.sync({ alter: false, force: false })
         console.log("Database synchronized successfully!");
     } catch (error) {
         console.error("Error syncing database:", error);
