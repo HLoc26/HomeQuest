becae1f9b370a24a7e55ec406d3b5fbf611611e9
HLoc26 <dhl26052004@gmail.com>
2025-02-16 20:56:11 +0700

    feat: routing for login and register

---
+++ src/routes/index.routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { auth } from "../controllers/index.controller.js";
+
+const router = Router();
+
+router.post("/login", auth.login);
+router.post("/register", auth.register);
+
+export default router;
+
