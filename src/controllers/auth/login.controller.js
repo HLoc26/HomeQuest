7616a23eef45531e706075414046919bdf808ca8
HLoc26 <dhl26052004@gmail.com>
2025-02-16 22:30:58 +0700

    feat: login controller

--- src/controllers/auth/login.controller.js
+++ src/controllers/auth/login.controller.js
@@ -1,3 +1,33 @@
-export function postLogin(req, res) {
-    res.send("PostLogin")
+import { AuthService } from "../../services/index.js"
+
+export async function postLogin(req, res) {
+    const { usn, pwd } = req.body
+
+    try {
+        if (!usn || !pwd) {
+            throw new SyntaxError("Missing username or password")
+        }
+
+        const { token, user } = await AuthService.login({ usn, pwd })
+
+        res.status(200).json({
+            success: true,
+            token,
+            user
+        })
+
+    } catch (error) {
+        if (error instanceof SyntaxError) {
+            res.status(400).json({
+                success: false,
+                message: error.message
+            })
+        }
+        else {
+            res.status(500).json({
+                success: false,
+                message: error.message
+            })
+        }
+    }
 }
