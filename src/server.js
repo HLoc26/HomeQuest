4d2fb38ff10747e646d86aaaab4fa2464ccaa322
HLoc26 <dhl26052004@gmail.com>
2025-02-16 23:47:19 +0700

    feat: config cors to accepts request from client

--- src/server.js
+++ src/server.js
@@ -2,11 +2,18 @@ import express from "express";
 import "dotenv/config"
 import syncDatabase from "./config/syncdb.js";
 import router from "./routes/index.routes.js";
+import cors from "cors"
 
 const PORT = process.env.PORT;
 const HOST = process.env.HOST_NAME;
 
 const app = express();
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"]
+}))
+
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
