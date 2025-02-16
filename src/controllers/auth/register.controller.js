79425c1d9fe320c422ca24b432e70a62050aeffe
HLoc26 <dhl26052004@gmail.com>
2025-02-16 22:19:42 +0700

    feat: register controller

--- src/controllers/auth/register.controller.js
+++ src/controllers/auth/register.controller.js
@@ -1,4 +1,19 @@
-export function postRegister(req, res) {
+import { AuthService } from "../../services/index.js";
+export async function postRegister(req, res) {
     const { usn, pwd } = req.body
-    res.json({ usn, pwd })
+    try {
+        const user = await AuthService.register({ usn, pwd })
+        if (user) {
+            res.status(200).json({
+                success: true,
+            })
+        } else {
+            throw new Error("Register failed: Internal server error");
+        }
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
 }
