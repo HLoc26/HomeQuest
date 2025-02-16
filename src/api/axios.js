ebd77401ee65e033e3b22deaea9096fd48b4ffc0
HLoc26 <dhl26052004@gmail.com>
2025-02-16 23:54:06 +0700

    feat: configurations for axios

---
+++ src/api/axios.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+// Cấu hình axios cho các yêu cầu đến API (cả Auth Server và Resource Server)
+const api = axios.create({
+    baseURL: "http://localhost:3000",  // Địa chỉ của server, có thể khác cho auth và resource server
+    headers: {
+        "Content-Type": "application/json", // Gửi dữ liệu dưới dạng JSON
+    },
+});
+
+export default api
