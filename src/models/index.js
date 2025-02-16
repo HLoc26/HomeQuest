e7ddbe1b2753140bd7a1cb0f3a725197904c4bf4
HLoc26 <dhl26052004@gmail.com>
2025-02-16 20:55:57 +0700

    feat: exporting model

---
+++ src/models/index.js
@@ -0,0 +1,4 @@
+export { default as Achievement } from "./Achievement.js"
+export { default as Attachment } from "./Attachment.js"
+export { default as Task } from "./Task.js"
+export { default as User } from "./User.js"
