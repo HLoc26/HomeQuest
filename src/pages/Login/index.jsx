e28a1524a1124cd4ad0462db3052d3aa14048afc
HLoc26 <dhl26052004@gmail.com>
2025-02-17 23:06:03 +0700

    Login page

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import api from "../../api/axios.js";
+
+function Login() {
+	const [inputField, setInput] = useState({ usn: "", pwd: "" });
+	const [errorText, setErrorText] = useState("");
+	const [showError, setShowError] = useState(false);
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		const user = { usn: inputField.usn, pwd: inputField.pwd };
+
+		const response = await api.post("/login", user, { withCredentials: true });
+		if (response.data.success) {
+			console.log(response.data);
+		} else {
+			setErrorText("Invalid username or password");
+			setShowError(true);
+		}
+	};
+
+	return (
+		<>
+			<form action="" onSubmit={handleSubmit}>
+				<input
+					type="text"
+					value={inputField.usn}
+					onChange={(e) => {
+						setShowError(false);
+						setInput({ ...inputField, usn: e.target.value });
+					}}
+					placeholder="Username"
+				/>
+				<br />
+				<input
+					type="text" /* Should be type="password" */
+					value={inputField.pwd}
+					onChange={(e) => {
+						setShowError(false);
+						setInput({ ...inputField, pwd: e.target.value });
+					}}
+					placeholder="Password"
+				/>
+				<br />
+				{showError && <p style={{ color: "red" }}>{errorText}</p>}
+				<button type="submit">Login</button>
+			</form>
+		</>
+	);
+}
+
+export default Login;
