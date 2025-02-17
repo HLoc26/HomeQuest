09e50e802a23493ed548f0a95ea274d1eeea5003
HLoc26 <dhl26052004@gmail.com>
2025-02-17 23:05:14 +0700

    feat: mock reducer file

---
+++ src/store/reducer.js
@@ -0,0 +1,18 @@
+import { LOGIN, LOGOUT } from "./constants"
+const initState = {
+    user: null,
+}
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case LOGIN:
+            break;
+        case LOGOUT:
+            break;
+        default:
+            throw new Error("Invalid action");
+    }
+}
+
+export { initState };
+export default reducer;
