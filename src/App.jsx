df32f7c29c17f72f4623c5f573d60b9dab8ed375
HLoc26 <dhl26052004@gmail.com>
2025-02-18 08:00:26 +0700

    feat: defining routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+
 import "./App.css";
 import Login from "./pages/Login";
+import Profile from "./pages/Profile";
 
 function App() {
 	return (
 		<>
-			<Login></Login>
+			<Routes>
+				<Route path="/login" element={<Login />} />
+				<Route path="/user" element={<Profile />} />
+			</Routes>
 		</>
 	);
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-// import { StrictMode } from 'react'
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+
 import "./index.css";
 import App from "./App.jsx";
 import { GlobalProvider } from "./store";
 
 createRoot(document.getElementById("root")).render(
-	// <StrictMode>
-	<GlobalProvider>
-		<App />
-	</GlobalProvider>
-	// </StrictMode>,
+	<StrictMode>
+		<GlobalProvider>
+			<Router>
+				<App />
+			</Router>
+		</GlobalProvider>
+	</StrictMode>
 );
