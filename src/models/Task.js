4dfafb02a3aab2a8f976b6d24422162534016499
HLoc26 <dhl26052004@gmail.com>
2025-02-20 08:34:19 +0700

    feat: update Task's status values

--- src/models/Task.js
+++ src/models/Task.js
@@ -8,7 +8,7 @@ Task.init(
 		id: { type: DataTypes.INTEGER.UNSIGNED, autoIncrement: true, primaryKey: true },
 		title: { type: DataTypes.STRING },
 		description: { type: DataTypes.STRING },
-		status: { type: DataTypes.ENUM("DONE", "ASSIGNED") },
+		status: { type: DataTypes.ENUM("DONE", "ASSIGNED", "PENDING") },
 		type: { type: DataTypes.ENUM("DAILY", "WEEKLY", "MONTHLY", "EPIC") },
 		difficulty: { type: DataTypes.ENUM("EASY", "MEDIUM", "HARD") },
 		assigned_to: { type: DataTypes.INTEGER.UNSIGNED },
@@ -18,7 +18,7 @@ Task.init(
 		created_by: { type: DataTypes.INTEGER.UNSIGNED },
 		proof_status: { type: DataTypes.ENUM("PENDING", "APPROVED", "REQUIRE_MORE") },
 	},
-	{ sequelize, timestamps: false }
+	{ sequelize, timestamps: true }
 );
 
 export default Task;
