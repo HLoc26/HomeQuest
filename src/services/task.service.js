d27d4863954966a4730649ae18be474af075a04d
HLoc26 <dhl26052004@gmail.com>
2025-02-20 10:09:43 +0700

    refactor: remove redundant async and await

--- src/services/task.service.js
+++ src/services/task.service.js
@@ -1,12 +1,12 @@
 import { Task } from "../models/index.js";
 
 class TaskService {
-	static async getAll() {
-		return await Task.findAll();
+	static getAll() {
+		return Task.findAll();
 	}
 
-	static async getAssigned(id) {
-		return await Task.findAll({ where: { assigned_to: id } });
+	static getAssigned(id) {
+		return Task.findAll({ where: { assigned_to: id } });
 	}
 
 	static getCreated(id) {
