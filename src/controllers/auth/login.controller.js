4f83b78a5587125600a0ede9ac628abcfac29381
HLoc26 <dhl26052004@gmail.com>
2025-02-20 10:54:37 +0700

    refactor: use common Error instead of SyntaxError

--- src/controllers/auth/login.controller.js
+++ src/controllers/auth/login.controller.js
@@ -5,7 +5,7 @@ export async function postLogin(req, res) {
 
 	try {
 		if (!usn || !pwd) {
-			throw new SyntaxError("Missing username or password");
+			throw new Error("Missing username or password");
 		}
 
 		const { token, user } = await AuthService.login({ usn, pwd });
@@ -21,16 +21,9 @@ export async function postLogin(req, res) {
 			user,
 		});
 	} catch (error) {
-		if (error instanceof SyntaxError) {
-			res.json({
-				success: false,
-				message: error.message,
-			});
-		} else {
-			res.json({
-				success: false,
-				message: error.message,
-			});
-		}
+		res.json({
+			success: false,
+			message: error.message,
+		});
 	}
 }
