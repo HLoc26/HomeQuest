e46af0f8224ee00584aeebf0f30f6764ca84a473
HLoc26 <dhl26052004@gmail.com>
2025-02-20 10:09:07 +0700

    feat: getting created tasks

--- src/services/task.service.js
+++ src/services/task.service.js
@@ -8,5 +8,9 @@ class TaskService {
 	static async getAssigned(id) {
 		return await Task.findAll({ where: { assigned_to: id } });
 	}
+
+	static getCreated(id) {
+		return Task.findAll({ where: { created_by: id } });
+	}
 }
 export default TaskService;
