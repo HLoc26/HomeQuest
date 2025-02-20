649169fedc7b0a28589ae862c8b474b1ed74c34a
HLoc26 <dhl26052004@gmail.com>
2025-02-20 09:33:46 +0700

    feat: update Task fields

--- src/models/Task.js
+++ src/models/Task.js
@@ -6,19 +6,51 @@ class Task extends Model {}
 Task.init(
 	{
 		id: { type: DataTypes.INTEGER.UNSIGNED, autoIncrement: true, primaryKey: true },
-		title: { type: DataTypes.STRING },
+		title: { type: DataTypes.STRING, allowNull: false },
 		description: { type: DataTypes.STRING },
-		status: { type: DataTypes.ENUM("DONE", "ASSIGNED", "PENDING") },
-		type: { type: DataTypes.ENUM("DAILY", "WEEKLY", "MONTHLY", "EPIC") },
-		difficulty: { type: DataTypes.ENUM("EASY", "MEDIUM", "HARD") },
-		assigned_to: { type: DataTypes.INTEGER.UNSIGNED },
-		xp_reward: { type: DataTypes.INTEGER.UNSIGNED },
-		gold_reward: { type: DataTypes.INTEGER.UNSIGNED },
-		proof_required: { type: DataTypes.BOOLEAN },
-		created_by: { type: DataTypes.INTEGER.UNSIGNED },
-		proof_status: { type: DataTypes.ENUM("PENDING", "APPROVED", "REQUIRE_MORE") },
+		status: {
+			type: DataTypes.ENUM("DONE", "ASSIGNED", "PENDING"),
+			allowNull: false,
+			defaultValue: "PENDING",
+		},
+		type: { type: DataTypes.ENUM("DAILY", "WEEKLY", "MONTHLY", "EPIC"), allowNull: false },
+		difficulty: { type: DataTypes.ENUM("EASY", "MEDIUM", "HARD"), allowNull: false },
+		assigned_to: {
+			type: DataTypes.INTEGER.UNSIGNED,
+			allowNull: true,
+			validate: {
+				isValidAssignment(value) {
+					if (this.status !== "PENDING" && value == null) {
+						throw new Error("Task đã được giao thì 'assigned_to' không thể là NULL.");
+					}
+					if (this.status === "PENDING" && value != null) {
+						throw new Error("Task ở trạng thái 'PENDING' thì 'assigned_to' phải là NULL.");
+					}
+				},
+			},
+		},
+		xp_reward: { type: DataTypes.INTEGER.UNSIGNED, allowNull: false },
+		gold_reward: { type: DataTypes.INTEGER.UNSIGNED, allowNull: false, defaultValue: 0 },
+		proof_required: { type: DataTypes.BOOLEAN, defaultValue: false },
+		created_by: { type: DataTypes.INTEGER.UNSIGNED, allowNull: false },
+		proof_status: {
+			type: DataTypes.ENUM("PENDING", "APPROVED", "REQUIRE_MORE"),
+			defaultValue: "PENDING",
+		},
 	},
-	{ sequelize, timestamps: true }
+	{
+		sequelize,
+		modelName: "task",
+		timestamps: true,
+		hooks: {
+			beforeValidate: (task) => {
+				const xpMapping = { EASY: 60, MEDIUM: 500, HARD: 1000 };
+				if (task.difficulty) {
+					task.xp_reward = xpMapping[task.difficulty] || 0;
+				}
+			},
+		},
+	}
 );
 
 export default Task;
