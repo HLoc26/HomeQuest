11978cfc4a3edba604c1d03373cf0a6d8cc125b6
HLoc26 <dhl26052004@gmail.com>
2025-02-20 09:00:22 +0700

    fix: token not defined

--- src/utils/decodeJwt.js
+++ src/utils/decodeJwt.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 import "dotenv/config";
-export function decode() {
+export function decode(token) {
 	const data = jwt.decode(token, process.env.JWT_SECRET);
 	return data;
 }
