df891cc528acd53130f2cc2c32f5bea6b88df0b8
HLoc26 <dhl26052004@gmail.com>
2025-02-20 08:17:46 +0700

    feat: handling error user null

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,11 +5,18 @@ export async function getUser(req, res) {
 	const token = req.cookies.jwt;
 	// console.log("token", token);
 
-	const user = jwt.decode(token, process.env.JWT_SECRET);
+	try {
+		const user = jwt.decode(token, process.env.JWT_SECRET);
 
-	console.log("HELLO", user);
-	res.json({
-		success: true,
-		user: { userId: user.userId, usn: user.usn },
-	});
+		console.log("HELLO", user);
+		res.json({
+			success: true,
+			user: { userId: user.userId, usn: user.usn },
+		});
+	} catch (error) {
+		res.json({
+			success: false,
+			message: "failed",
+		});
+	}
 }
