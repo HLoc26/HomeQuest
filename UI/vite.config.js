d169d02da3b72cc8dfe3decad769919fb6a13800
HLoc26 <dhl26052004@gmail.com>
2025-02-26 10:55:07 +0700

    refactor: adjust ports in container

--- UI/vite.config.js
+++ UI/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	server: {
+		host: "0.0.0.0", // Mở cổng để container có thể truy cập
+		port: 5173, // Giữ nguyên cổng
+		strictPort: true, // Giữ cổng 5173 cố định, tránh thay đổi ngẫu nhiên
+		watch: {
+			usePolling: true, // Đảm bảo theo dõi file đúng trong môi trường Docker
+		},
+	},
+});
