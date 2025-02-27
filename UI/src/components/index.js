11bd26e5418fee68d2262b8ffb3124cd22e18f9f
HLoc26 <dhl26052004@gmail.com>
2025-02-27 11:51:11 +0700

    feat: mass export for clean import

---
+++ UI/src/components/index.js
@@ -0,0 +1,9 @@
+// Xuất các thành phần từ thư mục con
+export { default as DescriptionBoard } from "./DescriptionBoard/DescriptionBoard.jsx";
+export { default as MainLayout } from "./Layout/MainLayout/MainLayout.jsx";
+export { default as NavBar } from "./Layout/NavBar/NavBar.jsx";
+export { default as ProtectedRoute } from "./ProtectedRoute/ProtectedRoute.jsx";
+export { default as RewardItem } from "./RewardItem/RewardItem.jsx";
+export { default as TaskItem } from "./TaskItem/TaskItem.jsx";
+export { default as TaskList } from "./TaskList/TaskList.jsx";
+export { default as ThemeButton } from "./ThemeButton/ThemeButton.jsx";
---
+++ UI/src/pages/index.js
@@ -0,0 +1,3 @@
+export { default as Dashboard } from "./Dashboard/index.jsx";
+export { default as Login } from "./Login/index.jsx";
+export { default as Profile } from "./Profile/index.jsx";
