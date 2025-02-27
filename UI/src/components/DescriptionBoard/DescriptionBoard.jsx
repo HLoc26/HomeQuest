8680346aee00784157a20ea10d90db46ece7b3e1
HLoc26 <dhl26052004@gmail.com>
2025-02-27 11:36:28 +0700

    feat: Description with reward

--- UI/src/components/DescriptionBoard/DescriptionBoard.jsx
+++ UI/src/components/DescriptionBoard/DescriptionBoard.jsx
@@ -1,9 +1,22 @@
+import RewardItem from "../RewardItem/RewardItem";
 import styles from "./DescriptionBoard.module.css";
 function DescriptionBoard({ task }) {
+	console.log(task);
 	return (
-		<div>
-			<h2>{task?.description ? task.description : "Chọn một nhiệm vụ để xem"}</h2>
-		</div>
+		<>
+			{task ? (
+				<div className={`px-3 w-100 ${styles.descriptionBoard}`}>
+					<h2 className={`${styles.description}`}>{task.description}</h2>
+					<hr style={{ width: "100%" }} />
+					<div className={`${styles.rewardPanel} d-flex`}>
+						<RewardItem type={"gold"} amount={task.gold_reward} />
+						<RewardItem type={"exp"} amount={task.xp_reward} />
+					</div>
+				</div>
+			) : (
+				<h2>{"Chọn một nhiệm vụ để xem"}</h2>
+			)}
+		</>
 	);
 }
 
