50279d64c8bf18ada7f2b065a9f44b153b4d824a
HLoc26 <dhl26052004@gmail.com>
2025-02-27 11:36:08 +0700

    feat: component RewardItem to display reward for task

---
+++ UI/src/components/RewardItem/RewardItem.jsx
@@ -0,0 +1,11 @@
+import goldImg from "../../assets/gold.png";
+import expImg from "../../assets/exp.png";
+function RewardItem({ type, amount }) {
+	return (
+		<div className="d-flex flex-column align-items-center">
+			{type == "gold" ? <img src={goldImg} alt="gold" width="40px" /> : <img src={expImg} alt="exp" width="40px" />}
+			<h3>{amount}</h3>
+		</div>
+	);
+}
+export default RewardItem;
