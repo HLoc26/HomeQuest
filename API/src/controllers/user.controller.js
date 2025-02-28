8c90a94c25fd1a8d9fbc3c7e86a707509d1fd7ef
HLoc26 <dhl26052004@gmail.com>
2025-02-28 17:53:15 +0700

    feat: handler for getting xp requirements

--- API/src/controllers/user.controller.js
+++ API/src/controllers/user.controller.js
@@ -1,12 +1,17 @@
-import { decode } from "../utils/decodeJwt.js";
 import "dotenv/config";
 import { handleRequest } from "../utils/handleRequest.js";
-
+import { decode } from "../utils/decodeJwt.js";
+import xpReqs from "../data/levelRequirements.js";
 export const getUser = handleRequest(async (req) => {
 	const token = req.cookies.jwt;
 	// console.log("token", token);
 	const user = decode(token);
-	console.log("HELLO", user);
+	// console.log("HELLO", user);
 	const ret = { userId: user.userId, usn: user.usn };
 	return ret;
+});
+
+export const getXpReqs = handleRequest(async () => {
+	// console.log(xpReqs);
+	return xpReqs;
 });
--- API/src/routes/user.routes.js
+++ API/src/routes/user.routes.js
@@ -3,5 +3,6 @@ import * as userController from "../controllers/user.controller.js";
 const userRouter = Router();
 
 userRouter.get("/me", userController.getUser);
+userRouter.get("/xpReqs", userController.getXpReqs);
 
 export default userRouter;
