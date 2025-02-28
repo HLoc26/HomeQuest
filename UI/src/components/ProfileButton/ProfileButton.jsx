f79c5dfce8a32e835b2e57894234d8b1a249583c
HLoc26 <dhl26052004@gmail.com>
2025-02-28 17:09:31 +0700

    feat: ProfileButton component

---
+++ UI/src/components/ProfileButton/ProfileButton.jsx
@@ -0,0 +1,43 @@
+import { ProgressBar } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Dropdown from "react-bootstrap/Dropdown";
+import { useStore } from "~/store";
+import styles from "./ProfileButton.module.css";
+
+function ProfileButton() {
+	const [state, dispatch] = useStore();
+	const user = state.user;
+	const theme = state.theme;
+
+	// Experience value
+	const experience = 60;
+
+	return (
+		<Dropdown as={ButtonGroup} className="profile-button-group border">
+			<Button href="/user/me" variant={theme} className={`${styles.userBtn} d-flex align-items-center border`}>
+				<div style={{ minWidth: "150px" }}>
+					<div className={styles.userName}>{user.usn}</div>
+					<ProgressBar
+						now={experience}
+						label={`${experience}%`}
+						variant="information"
+						style={{
+							height: "15px",
+						}}
+					/>
+				</div>
+			</Button>
+
+			<Dropdown.Toggle split variant={theme} id="dropdown-split-basic" style={{ padding: "0.5rem 0.75rem" }} />
+
+			<Dropdown.Menu>
+				<Dropdown.Item href="/tasks">Your tasks</Dropdown.Item>
+				<Dropdown.Divider />
+				<Dropdown.Item href="/logout">Log out</Dropdown.Item>
+			</Dropdown.Menu>
+		</Dropdown>
+	);
+}
+
+export default ProfileButton;
