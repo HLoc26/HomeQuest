ae7f57dd0e2486aca0587855f5d5d2c127ed0583
HLoc26 <dhl26052004@gmail.com>
2025-03-01 11:17:11 +0700

    feat: Add button go assign task

--- UI/src/components/DescriptionBoard/DescriptionBoard.jsx
+++ UI/src/components/DescriptionBoard/DescriptionBoard.jsx
@@ -1,7 +1,9 @@
+import { Button } from "react-bootstrap";
+
 import RewardItem from "~/components/RewardItem/RewardItem";
 import styles from "./DescriptionBoard.module.css";
 function DescriptionBoard({ task }) {
-	// console.log(task);
+	console.log(task);
 	return (
 		<>
 			{task ? (
@@ -9,8 +11,11 @@ function DescriptionBoard({ task }) {
 					<h2 className={`${styles.description}`}>{task.description}</h2>
 					<hr style={{ width: "100%" }} />
 					<div className={`${styles.rewardPanel} d-flex`}>
-						<RewardItem type={"gold"} amount={task.gold_reward} />
-						<RewardItem type={"exp"} amount={task.xp_reward} />
+						<div className={`${styles.rewardPanel} d-flex flex-grow-1`}>
+							<RewardItem type={"gold"} amount={task.gold_reward} />
+							<RewardItem type={"exp"} amount={task.xp_reward} />
+						</div>
+						<Button className="rounded-pill px-3 py-2 btn btn-outline-light my-3">Nhận nhiệm vụ</Button>
 					</div>
 				</div>
 			) : (
