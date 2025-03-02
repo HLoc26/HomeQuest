7e9868f3b6382cf7d7693ccb70e013f7e5abe439
HLoc26 <dhl26052004@gmail.com>
2025-03-02 18:52:06 +0700

    feat: assigning task

--- UI/src/components/TaskInteractionButtons/AssignButton.jsx
+++ UI/src/components/TaskInteractionButtons/AssignButton.jsx
@@ -1,9 +1,55 @@
+import { useCallback } from "react";
 import { Button } from "react-bootstrap";
 
 import axios from "~/api/axios.js";
+import { useStore } from "~/store";
 
-function AssignButton() {
-	const handleAssign = () => {};
+function AssignButton({ task, setter }) {
+	const [state, dispatch] = useStore();
+
+	const user = state.user;
+
+	const handleAssign = useCallback(async () => {
+		try {
+			const response = await axios.post(
+				"/task/assign",
+				{
+					userId: user.userId,
+					taskId: task.id,
+				},
+				{
+					withCredentials: true,
+				}
+			);
+			if (response.data.success) {
+				const newTaskData = { ...task, status: "ASSIGNED", assigned_to: user.userId };
+
+				setter.setAllTasks((prev) => {
+					prev.filter((t) => t.id !== task.id);
+				});
+
+				setter.setAssignedTasks((prev) => {
+					if (prev.some((t) => t.id === task.id)) {
+						return prev.map((t) => (t.id === task.id ? newTaskData : t));
+					} else {
+						return [...prev, newTaskData];
+					}
+				});
+
+				setter.setCreatedTasks((prev) => {
+					if (prev.some((t) => t.id === task.id)) {
+						return prev.map((t) => (t.id === task.id ? newTaskData : t));
+					} else {
+						return prev;
+					}
+				});
+
+				setter.setSelectedTask(newTaskData);
+			}
+		} catch (error) {
+			console.error("Lỗi khi nhận nhiệm vụ:", error);
+		}
+	}, [user, task, setter]);
 
 	return (
 		<Button className="rounded-pill px-3 py-2 btn btn-outline-light my-3" onClick={handleAssign}>
