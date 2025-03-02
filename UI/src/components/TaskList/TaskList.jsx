81e1b0200889d67a290c3d4d18ecdf4da92ee295
HLoc26 <dhl26052004@gmail.com>
2025-03-02 18:09:15 +0700

    refactor: cleanning up code

--- UI/src/components/TaskList/TaskList.jsx
+++ UI/src/components/TaskList/TaskList.jsx
@@ -7,16 +7,20 @@ import TaskItem from "~/components/TaskItem/TaskItem";
 function TaskList({ eventKey, listName, tasks, emptyText, onTaskSelect }) {
 	// prettier-ignore
 	return (
-		<>
-				<Accordion.Item eventKey={eventKey}>
-					<Accordion.Header>{listName}</Accordion.Header>
-					<Accordion.Body className={styles.taskList}>
-					{tasks instanceof Array && tasks.length > 0 ?
-						tasks.map((task) => <TaskItem key={task.id} task={task} setSelectedTask={onTaskSelect} />)
-						: <p>{emptyText}</p>}
-					</Accordion.Body>
-				</Accordion.Item>
-		</>
+		<Accordion.Item eventKey={eventKey}>
+			<Accordion.Header>{listName}</Accordion.Header>
+			<Accordion.Body className={styles.taskList}>
+				{tasks?.length > 0
+					? tasks.map((task) =>
+						<TaskItem
+							key={task.id}
+							task={task}
+							setSelectedTask={onTaskSelect}
+						/>)
+					: <p>{emptyText}</p>
+				}
+			</Accordion.Body>
+		</Accordion.Item>
 	);
 }
 
