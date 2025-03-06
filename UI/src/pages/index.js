e8d7e4e205e46a4bbed5c24652bdaabed5878fda
HLoc26 <dhl26052004@gmail.com>
2025-03-07 00:10:11 +0700

    feat: add Logout and CreateTask components to index export

--- UI/src/pages/index.js
+++ UI/src/pages/index.js
@@ -1,3 +1,5 @@
 export { default as Dashboard } from "./Dashboard/index.jsx";
 export { default as Login } from "./Login/index.jsx";
 export { default as Profile } from "./Profile/index.jsx";
+export { default as Logout } from "./Logout/index.jsx";
+export { default as CreateTask } from "./CreateTask/index.jsx";
