e730a21961609ae40b02b83e77991619d4769230
HLoc26 <dhl26052004@gmail.com>
2025-03-11 15:36:32 +0700

    feat: enhance multer filename to include original file extension

--- API/src/config/multer.js
+++ API/src/config/multer.js
@@ -1,10 +1,14 @@
 import multer from "multer";
+import path from "path";
+
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
 		cb(null, "uploads");
 	},
 	filename: function (req, file, cb) {
-		cb(null, file.fieldname + "-" + Date.now());
+		// Lấy extension của file gốc
+		const ext = path.extname(file.originalname);
+		cb(null, file.fieldname + "-" + Date.now() + ext);
 	},
 });
 
